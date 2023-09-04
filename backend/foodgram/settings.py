0dfd45a9f089cdf061c1a992cb4b14f5fe7d8e1f
Альбина Гильмутдинова <inovaras@mail.ru>
2023-09-04 12:46:38 +0500

    added apps users and recipes

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'recipes.apps.RecipesConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
