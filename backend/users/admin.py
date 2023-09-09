c1aedef84d8210f4bc21e6ff2d55b4f6ee2539a0
Альбина Гильмутдинова <inovaras@mail.ru>
2023-09-09 16:24:31 +0500

    added follower to admin tools

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Follow
 
 admin.site.register(User, UserAdmin)
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'following')
+    list_filter = ('user', 'following')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Follow, FollowAdmin)
